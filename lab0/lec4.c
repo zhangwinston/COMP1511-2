d9257ccfb846a7276617dd868a1e3e0065db122d
williamgao-dev <35654586+williamgao-dev@users.noreply.github.com>
2018-03-11 16:34:35 +1100

    lab0 - COMP1511
    lab0/module0 files for COMP1511

---
+++ lab0/lec4.c
@@ -0,0 +1,20 @@
+// program to demo writing c
+// by: William Gao
+// date: 3 march 18
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int main (int argc, char* argv[]){
+	int x;
+	x = 9001;
+
+	printf("Hello Julian whereever you are...\n");
+	printf("My favourite number is ");
+	if (x > 9000){
+	printf("I like cheese\n");
+}
+	printf("%d\n",x);
+
+	return EXIT_SUCCESS;
+}
