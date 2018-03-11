cfc656f6b1ac5570241353f85ba7c3a99965203f
williamgao-dev <35654586+williamgao-dev@users.noreply.github.com>
2018-03-11 16:29:31 +1100

    Lab0 - COMP1511
    Lab0/Module0 Activities for COMP1511 (HS)

---
+++ first.c
@@ -0,0 +1,10 @@
+// by William Gao
+// 3/03/18
+// demo program - prints a very important message!
+#include <stdio.h>
+int main (int argc, char*argv[]){
+
+
+	printf("Hello World\n");
+	return 42;
+}
